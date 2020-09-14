3d37c231b07825af620aee0c06e630dc2843b927
Bill Wei <bilwei@redhat.com>
2020-09-14 13:22:40 -0400

    Setup proxy for blue button API

---
+++ bbproxy.js
@@ -0,0 +1,56 @@
+var ClientOAuth2 = require('client-oauth2')
+ 
+var tokens = {};
+
+var querystring = require('querystring');
+var url = require('url');
+var http = require('http');
+
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  const token = tokens[req.headers['x-bb-uid']]
+  proxyReq.setHeader('Authorization', 'Bearer ' + token);
+});
+
+function getOAuthClient(state) {
+  return new ClientOAuth2({
+    clientId: process.env.BB_ID,
+    clientSecret: process.env.BB_SECRET,
+    accessTokenUri: 'https://sandbox.bluebutton.cms.gov/v1/o/token/',
+    authorizationUri: 'https://sandbox.bluebutton.cms.gov/v1/o/authorize/',
+    redirectUri: 'http://localhost:3000/callback',
+    scopes: ['patient/Patient.read', 'patient/Coverage.read', 'patient/ExplanationOfBenefit.read', 'profile'],
+    state: state
+  });
+}
+
+http.createServer(function (req, res) {
+  const parts = url.parse(req.url);
+  const route = parts.pathname;
+
+  if (route == '/login') {
+    const query = querystring.parse(parts.query);
+    const authorizationUri = getOAuthClient(query['uid']).code.getUri();
+
+    res.writeHead(301, { "Location": authorizationUri});
+    res.end();
+  }
+  else if(route == '/callback') {
+    const query = querystring.parse(parts.query);
+    getOAuthClient(query['state']).code.getToken(req.url)
+      .then(function(user) {
+        console.log(user);
+        tokens[user.client.options['state']] = user.accessToken;
+        res.write('Login succeeded!')
+        res.end();
+      });
+  }
+  else {
+    proxy.web(req, res, {
+      target: 'https://sandbox.bluebutton.cms.gov',
+      changeOrigin: true
+    });
+  }
+}).listen(3000);
